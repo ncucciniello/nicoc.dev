10cbb3f686443aacb2fb8ff780d7ef658ee5d36b
Nico Cucciniello <ncucciniello@gmail.com>
2016-12-23 18:00:31 -0500

    add email link, message on cursor hover

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-// document.querySelector('.blink').onMouseOver="console.log('moused over')";
+function swag() {
+  // Type Effect
+  let text = "";
+  let count = 0;
+  let maxspeed = 125;
 
+  function typeit(effect) {
+    text = effect;
+    type();
+  }
 
-// function contactMe() {
-//   document.querySelctor('.name').innerHTML="Coontact Me "
-// }
+  function character(start, end, text) {
+    return text.substring(start, end);
+  }
+
+  function type() {
+    let random = Math.floor(Math.random() * maxspeed);
+    setTimeout(type, random);
+    document.querySelector('.name').append(character(count, count+1, text));
+    count++;
+  }
+
+  typeit(' - Available for hire');
+  document.querySelector('.blink').removeAttribute('onmouseover');
+}
